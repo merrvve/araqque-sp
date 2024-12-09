39a5df79d86e018bccbb5bba1d816668c38884e7
mervs <mervsen@gmail.com>
2024-12-09 10:16:43 +0300

    headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+    trustedHeaders: true, // Enables support for trusting proxy headers like x-forwarded-host
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Match all routes
+        headers: [
+          { key: "x-forwarded-host", value: "araqque.netlify.app" },
+        ],
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
