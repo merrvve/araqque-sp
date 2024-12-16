4904eb4d2783fa97e447f9a6085cc115769dcf05
mervs <mervsen@gmail.com>
2024-12-16 15:48:23 +0300

    list homeworks

--- components/trainer/Homeworks.tsx
+++ components/trainer/Homeworks.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { Homework } from "@/types/Homework";
+
 type Props = {
     user: any;
-    homeworks: any;
+    homeworks: Homework[];
   };
 
 const Homeworks: React.FC<Props> = ({ user, homeworks }) => {
@@ -10,7 +12,30 @@ const Homeworks: React.FC<Props> = ({ user, homeworks }) => {
     
     return (
         <>
-        Ödevler
+        <h1 className="text-lg font-semibold m-2" >Ödevler</h1>
+        <div>
+            <table>
+                <thead>
+                    <th>Ödev Adı</th>
+                    <th>Ödev Kodu</th>
+                    <th>Değerlendirme Sonuçları</th>
+                </thead>
+                <tbody>
+                    {
+                        homeworks.map((homework:Homework)=> 
+                            <tr>
+                                 <td>{homework.name}</td>
+                                 <td>{homework.id}</td>
+                                 <td></td>
+
+                            </tr>
+                           
+                           
+                        )
+                    }
+                </tbody>
+            </table>
+        </div>
         </>
     )
  }
